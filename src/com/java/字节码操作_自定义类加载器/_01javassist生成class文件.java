4bb63bd65df1dbcf29f730c965063aa9d4b82314
sunweiye <794982825@qq.com>
2019-06-18 10:27:01 +0800

    12

--- src/com/java/字节码操作_自定义类加载器/_01javassist生成class文件.java
+++ src/com/java/字节码操作_自定义类加载器/_01javassist生成class文件.java
@@ -7,6 +7,9 @@ import javassist.CtNewMethod;
 /*
  * javassist 是一种字节码操纵技术
  *  在程序运行期,直接通过程序生成所需的class文件,存储到特定的位置
+ *  1.通过字节码操纵技术生成class文件  -->(可以通过反编译技术生成Java文件(javap或者工具 jd-gui))
+ *  2.通过自定义的类加载器将class文件加载到JVM中
+ *  3.在Test中可以调用反射技术来生成此对象
  */
 public class _01javassist生成class文件 {
 	
